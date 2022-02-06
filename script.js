1a13f0e171a0fb7c04e6040be0faab8de6eecd53
Khadija Shahid <khadija_shahid3197@outlook.com>
2022-02-06 16:18:39 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,95 @@
+if (window.localStorage) { // checks if browser support localStorage
+    if (localStorage['testKey']) { // checks if value exists
+        console.log('Value exist on page load in localstorage for key testKey : ', localStorage['testKey']);
+    }
+    localStorage['testKey'] = 'Hi again!'; // stores value in localstorage
+} else {
+    console.log('your browser dont support localstorage');
+}
+
+
+const text = document.getElementById("text");
+const addTaskButton = document.getElementById("add-task-btn");
+const saveTaskButton = document.getElementById("save-todo-btn");
+const listBox = document.getElementById("listBox");
+const saveInd = document.getElementById("saveIndex");
+
+let todo = [];
+addTaskButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    let todoArray = localStorage.getItem("todoArray");
+    if (todoArray === null) {
+        todo = [];
+        alert("Please Enter a Task")
+    } else {
+
+        todo = JSON.parse(todoArray);
+    }
+    todo.push(text.value);
+    text.value = "";
+    localStorage.setItem("todoArray", JSON.stringify(todo));
+    displayTodo();
+
+});
+
+
+function displayTodo() {
+    let todoArray = localStorage.getItem("todoArray");
+    if (todoArray === null) {
+        todo = [];
+        alert("Please Enter a Task")
+    } else {
+
+        todo = JSON.parse(todoArray);
+    }
+    let htmlCode = "";
+    todo.forEach((list, ind) => {
+        htmlCode += `<div id="task">
+        <div id="listTask"> 
+         <p id="taskname">${list}</p>
+         </div>
+
+   <button id="edit" onclick='edit(${ind})'><i class="fas fa-edit"></i></button>
+   <button id="delete" onclick='deleteTodo(${ind})' ><i class="fas fa-trash" > </i></button>
+  
+</div>`;
+    });
+    listBox.innerHTML = htmlCode;
+}
+
+function deleteTodo(ind) {
+    let todoArray = localStorage.getItem("todoArray");
+    todo = JSON.parse(todoArray);
+    todo.splice(ind, 1);
+    localStorage.setItem("todoArray", JSON.stringify(todo));
+    displayTodo();
+}
+
+function edit(ind) {
+    saveInd.value = ind;
+    let todoArray = localStorage.getItem("todoArray");
+    todo = JSON.parse(todoArray);
+    text.value = todo[ind];
+    addTaskButton.style.display = "none";
+    saveTaskButton.style.display = "block";
+
+}
+
+
+saveTaskButton.addEventListener("click", () => {
+    let todoArray = localStorage.getItem("todoArray");
+    todo = JSON.parse(todoArray);
+    let id = saveInd.value;
+    todo[id] = text.value;
+    addTaskButton.style.display = "block";
+    saveTaskButton.style.display = "none";
+    text.value = "";
+    localStorage.setItem("todoArray", JSON.stringify(todo));
+    displayTodo();
+});
+
+// to clear the local storage
+document.querySelector('#btn').onclick = function() {
+    alert("to clear local storage")
+    localStorage.clear();
+}
